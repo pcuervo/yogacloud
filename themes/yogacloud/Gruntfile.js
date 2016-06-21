62e4a1fa58d610a46333fdb5cdc5225e968e8146
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-21 17:05:22 -0500

    borde right

--- themes/yogacloud/Gruntfile.js
+++ themes/yogacloud/Gruntfile.js
@@ -1,64 +1,64 @@
 (function () {
-   'use strict';
+  'use strict';
 }());
 module.exports = function(grunt) {
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+   grunt.initConfig({
+       pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            options: {
-                separator: 'rnrn'
-            },
-            dist: {
-                src: ['assets/js/bin/materialize.js'],
-                dest: 'assets/js/main.js'
-            }
-        },
+       concat: {
+           options: {
+               separator: 'rnrn'
+           },
+           dist: {
+               src: ['assets/js/bin/materialize.js'],
+               dest: 'assets/js/main.js'
+           }
+       },
 
-        uglify: {
-            options: {
-                banner: '/*! &lt;%= pkg.name %&gt; &lt;%= grunt.template.today("dd-mm-yyyy") %&gt; */n'
-            },
-            dist: {
-                files: {
-                    'assets/js/main.min.js': ['&lt;%= concat.dist.dest %&gt;']
-                }
-            }
-        },
+       uglify: {
+           options: {
+               banner: '/*! &lt;%= pkg.name %&gt; &lt;%= grunt.template.today("dd-mm-yyyy") %&gt; */n'
+           },
+           dist: {
+               files: {
+                   'assets/js/main.min.js': ['&lt;%= concat.dist.dest %&gt;']
+               }
+           }
+       },
 
-        jshint: {
-            files: ['gruntfile.js', 'assets/js/*.js', 'assets/js/modules/*.js'],
-            options: {
-                globals: {
-                    jQuery: true,
-                    console: true,
-                    module: true
-                }
-            }
-        },
+       jshint: {
+           files: ['gruntfile.js', 'assets/js/*.js', 'assets/js/modules/*.js'],
+           options: {
+               globals: {
+                   jQuery: true,
+                   console: true,
+                   module: true
+               }
+           }
+       },
 
-        compass: {
-            dist: {
-                options: {
-                    sassDir: 'sass',
-                    cssDir: '../yogacloud',
-                    environment: 'development',
-                    outputStyle: 'compressed'
-                }
-            }
-        },
+       compass: {
+           dist: {
+               options: {
+                   sassDir: 'sass',
+                   cssDir: '../yogacloud',
+                   environment: 'development',
+                   outputStyle: 'compressed'
+               }
+           }
+       },
 
-        watch: {
-            files: ['&lt;%= jshint.files %&gt;', 'sass/**/*.scss'],
-            tasks: ['concat', 'uglify', 'jshint', 'compass']
-        }
+       watch: {
+           files: ['&lt;%= jshint.files %&gt;', 'sass/**/*.scss'],
+           tasks: ['concat', 'uglify', 'jshint', 'compass']
+       }
 
-    });
+   });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['concat', 'uglify', 'jshint', 'compass', 'watch']);
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-contrib-compass');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.registerTask('default', ['concat', 'uglify', 'jshint', 'compass', 'watch']);
 };
--- themes/yogacloud/page-curso.php
+++ themes/yogacloud/page-curso.php
@@ -1,8 +1,79 @@
 <?php get_header(); the_post();  ?>
+	<div class="[ main-banner ]" style="background-size: cover; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+		<div class="[ gradient-linear-opacity ]">
+			<div class="[ container ]">
+				<div class="[ row ]">
+					<div class="[ col s12 ][ white-text text-center ]">
+						<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
+						<h2 class="[ padding-sides ]"> Primum in nostrane potestate est quid meminerimus duo.</h2>
+						<div class="[ btn-main-cta ][ relative ][ padding ]">
+							<a class="[ btn btn-rounded ][ absolute ]">tomar curso - $1200</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 
-
-
-
-
+	<section class="[ container ]">
+		<article>
+			<div class="[ row ][ text-center ]">
+				<div class="[ col s4 ][ border-right--dark ]">
+					<p>30</p>
+					<p>lecciones</p>
+				</div>
+				<div class="[ col s4 ][ border-right--dark ]">
+					<p>1</p>
+					<p>por semana</p>
+				</div>
+				<div class="[ col s4 ]">
+					<p>50</p>
+					<p>horas</p>
+				</div>
+			</div>
+			<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing</p>
+			<a class="[ btn btn-rounded ]">regalar curso</a>
+		</article>
+		<article>
+			<h5 class="[ text-center ]">Impartido por</h5>
+			<div class="[ row ]">
+				<div class="[ col s6 ]">
+					<p>Juan O'Donoju</p>
+					<a class="[ btn btn-rounded ]">ver más</a>
+				</div>
+				<div class="[ col s6 ]">
+					<p>Juan O'Donoju</p>
+					<a class="[ btn btn-rounded ]">ver más</a>
+				</div>
+			</div>
+		</article>
+		<article>
+			<h4 class="[ text-center ]">Módulos</h4>
+			<div>
+				<h5>Módulo 1</h5>
+				<p>Fortemne possumus dicere eundem illum torquatum quid quod.</p>
+				<a class="[ btn btn-rounded ]">ver más</a>
+			</div>
+			<div>
+				<h5>Módulo 2</h5>
+				<p>Fortemne possumus dicere eundem illum torquatum quid quod.</p>
+			</div>
+			<div>
+				<h5>Módulo 3</h5>
+				<p>Fortemne possumus dicere eundem illum torquatum quid quod.</p>
+			</div>
+			<div>
+				<h5>Módulo 4</h5>
+				<p>Fortemne possumus dicere eundem illum torquatum quid quod.</p>
+			</div>
+		</article>
+		<article>
+			<h4 class="[ text-center ]">Rating</h4>
+			<a class="[ btn btn-rounded ]">ver comentarios</a>
+		</article>
+		<article>
+			<h4 class="[ text-center ]">Compartelo</h4>
+		</article>
+	</section>
 
 <?php get_footer(); ?>
