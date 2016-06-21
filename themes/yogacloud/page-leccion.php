f770e295e793750c75fb1c569be8076af611056c
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-21 18:04:17 -0500

    page leccion, faltan estilos

---
+++ themes/yogacloud/page-leccion.php
@@ -0,0 +1,37 @@
+<?php get_header(); the_post();  ?>
+
+	<div class="[ main-banner ]" style="background-size: cover; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+		<div class="[ gradient-linear-opacity ]">
+			<div class="[ container ]">
+				<div class="[ row ]">
+					<div class="[ col s12 ][ white-text text-center ]">
+						<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
+						<img src="<?php echo THEMEPATH; ?>images/play-button.png" alt="play button">
+						<div class="[ btn-main-cta ][ relative ][ padding ]">
+							<a class="[ btn btn-rounded ][ absolute ]">tomar curso - $1200</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<section class="[ container ]">
+		<article>
+			<h4 class="[ text-center ]">Lección 1 - Placet igitur tibi cato cum res sumpseris non concessas.</h4>
+			<p>Verum hoc loco sumo verbis his eandem certe vim voluptatis Epicurum nosse quam ceteros. Si enim ad populum me vocas, eum. Sed residamus, inquit, si placet. Cave putes quicquam esse verius. Duo Reges: constructio interrete.</p>
+			<img class="[ margin-auto ]" src="<?php echo THEMEPATH; ?>images/sky.png" alt="sky image">
+			<p>Portenta haec esse dicit, neque ea ratione ullo modo posse vivi; Quid, si non sensus modo ei sit datus, verum etiam animus hominis? Quamvis enim depravatae non sint, pravae tamen esse possunt. Egone quaeris, inquit, quid sentiam? [redacted]tilio Rufo.</p>
+			<div class="[ text-center ]">
+				<a class="[ btn btn-rounded ]">
+					<img class="[ middle inline-blok ]" src="<?php echo THEMEPATH; ?>images/download.png" alt="download image">
+					<span class="[ middle inline-blok ]">descargar notas</span>
+				</a>
+			</div>
+		</article>
+	</section>
+
+
+
+
+<?php get_footer(); ?>
