c5aa9b76dc5c1b29fbb40ce1d0be8d92e7fbf662
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-21 18:25:54 -0500

    tienda, faltan estilos

--- themes/yogacloud/inc/pages.php
+++ themes/yogacloud/inc/pages.php
@@ -53,4 +53,15 @@
 			wp_insert_post( $page, true );
 		}
 
+		if( ! get_page_by_path('tienda') ){
+			$page = array(
+				'post_author' => 1,
+				'post_status' => 'publish',
+				'post_title'  => 'Tienda',
+				'post_name'   => 'tienda',
+				'post_type'   => 'page'
+			);
+			wp_insert_post( $page, true );
+		}
+
 	});
---
+++ themes/yogacloud/page-tienda.php
@@ -0,0 +1,62 @@
+<?php get_header(); the_post();  ?>
+
+	<div class="[ main-banner ]" style="background-size: cover; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+		<div class="[ gradient-linear-opacity ]">
+			<div class="[ container ]">
+				<div class="[ row ]">
+					<div class="[ col s12 ][ white-text text-center ]">
+						<h1 class="[ padding-sides ]">Yoga cloud tienda</h1>
+						<h2 class="[ padding-sides ]"> Primum in nostrane potestate est quid meminerimus duo.</h2>
+						<div class="[ btn-main-cta ][ relative ][ padding ]">
+							<a class="[ btn btn-rounded ][ absolute ]">ver todos los productos</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<section class="[ container ]">
+		<h4 class="[ text-center ]">Lo más vendido</h4>
+		<article class="[ card ][ box-shadow--none ]">
+			<div class="[ card-image ]">
+				<img src="<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg" alt="Cursos Yogacloud">
+				<span class="[ card-title top--0 ]">Título del producto</span>
+			</div>
+			<div class="[ card-action ]">
+				<a class="[ btn btn-rounded ][ absolute ][ left-30 ]">comprar - $900</a>
+			</div>
+		</article>
+		<article class="[ card ][ box-shadow--none ]">
+			<div class="[ card-image ]">
+				<img src="<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg" alt="productos Yogacloud">
+				<span class="[ card-title top--0 ]">Título del curso</span>
+			</div>
+			<div class="[ card-action ]">
+				<a class="[ btn btn-rounded ][ absolute ][ left-30 ]">comprar - $900</a>
+			</div>
+		</article>
+		<article class="[ card ][ box-shadow--none ]">
+			<div class="[ card-image ]">
+				<img src="<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg" alt="productos Yogacloud">
+				<span class="[ card-title top--0 ]">Título del curso</span>
+			</div>
+			<div class="[ card-action ]">
+				<a class="[ btn btn-rounded ][ absolute ][ left-30 ]">comprar - $900</a>
+			</div>
+		</article>
+		<article class="[ card ][ box-shadow--none ]">
+			<div class="[ card-image ]">
+				<img src="<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg" alt="productos Yogacloud">
+				<span class="[ card-title top--0 ]">Título del curso</span>
+			</div>
+			<div class="[ card-action ]">
+				<a class="[ btn btn-rounded ][ absolute ][ left-30 ]">comprar - $900</a>
+			</div>
+		</article>
+	</section>
+
+
+
+
+
+<?php get_footer(); ?>
