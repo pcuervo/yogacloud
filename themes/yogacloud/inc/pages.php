bc7bc132398e0f94a02b496b4bf091947c7d31d0
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-21 16:28:39 -0500

    pages.php y page-curso

--- themes/yogacloud/inc/pages.php
+++ themes/yogacloud/inc/pages.php
@@ -18,6 +18,17 @@
 			);
 			wp_insert_post( $page, true );
 		}
-		
+
+
+		if( ! get_page_by_path('curso') ){
+			$page = array(
+				'post_author' => 1,
+				'post_status' => 'publish',
+				'post_title'  => 'Curso',
+				'post_name'   => 'curso',
+				'post_type'   => 'page'
+			);
+			wp_insert_post( $page, true );
+		}
 
 	});
---
+++ themes/yogacloud/page-curso.php
@@ -0,0 +1,8 @@
+<?php get_header(); the_post();  ?>
+
+
+
+
+
+
+<?php get_footer(); ?>
