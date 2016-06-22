e4abcb7a1c6490f02cd5bcd01bb199699895cdb8
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-22 11:07:19 -0500

    margenes y padding corregidos de footer

--- themes/yogacloud/footer.php
+++ themes/yogacloud/footer.php
@@ -1,26 +1,32 @@
 <?php global $result; ?>
 		<footer class="page-footer">
 			<div class="container">
-				<div class="row">
-					<div class="col l4 s12">
-						<h5 class="white-text">¿Necesitas ayuda?</h5>
-						<img src="<?php echo THEMEPATH; ?>icons/phone.png" alt="télefono">
-						<img src="<?php echo THEMEPATH; ?>icons/mail.png" alt="correo">
+				<div class="row [ border-bottom--light ][ padding-bottom ]">
+					<div class="col l4 s12 [ margin-bottom ]">
+						<h5 class="white-text [ no-margin-top ]">¿Necesitas ayuda?</h5>
+						<img class="[ padding-sides--xsmall ]" src="<?php echo THEMEPATH; ?>icons/phone.png" alt="télefono">
+						<img class="[ padding-sides--xsmall ]" src="<?php echo THEMEPATH; ?>icons/mail.png" alt="correo">
 					</div>
 					<div class="col l4 s12">
-						<h5 class="white-text">Seámos amigos</h5>
-						<img src="<?php echo THEMEPATH; ?>icons/twitter.png" alt="twitter">
-						<img src="<?php echo THEMEPATH; ?>icons/facebook.png" alt="facebook">
-						<img src="<?php echo THEMEPATH; ?>icons/instagram.png" alt="instagram">
+						<h5 class="white-text [ no-margin-top ]">Seámos amigos</h5>
+						<img class="[ padding-sides--xsmall ]" src="<?php echo THEMEPATH; ?>icons/twitter.png" alt="twitter">
+						<img class="[ padding-sides--xsmall ]" src="<?php echo THEMEPATH; ?>icons/facebook.png" alt="facebook">
+						<img class="[ padding-sides--xsmall ]" src="<?php echo THEMEPATH; ?>icons/instagram.png" alt="instagram">
 					</div>
 				</div>
 				<div class="col l4 s12">
 					<h5><img class="logo" src="<?php echo THEMEPATH; ?>images/logo-horizontal-light.png" alt="Logo yogacloud"></h5>
-					<ul>
-						<li><a class="grey-text text-lighten-3" href="#!">Aviso Legal</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Política de Cookies</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Política de Privacidad</a></li>
-					</ul>
+					<div class="[ row ]">
+						<div class="col s4">
+							<a class="grey-text text-lighten-3" href="#!">Aviso Legal</a>
+						</div>
+						<div class="col s4">
+							<a class="grey-text text-lighten-3" href="#!">Política de Cookies</a>
+						</div>
+						<div class="col s4">
+							<a class="grey-text text-lighten-3" href="#!">Política de Privacidad</a>
+						</div>
+					</div>
 				</div>
 				<div class="[ white-text ]">
 					© Copyright 2016
