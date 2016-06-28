8152c37cdeeda30a9cb9c34df69f63acb94c0f24
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-28 11:14:49 -0500

    leccion, margenes y tamaño sound

--- themes/yogacloud/page-leccion.php
+++ themes/yogacloud/page-leccion.php
@@ -1,82 +1,83 @@
 <?php get_header(); the_post();  ?>
+	<section>
+		<div class="slider [ video-leccion ]">
+			<ul class="slides">
+				<li>
 
-	<div class="slider [ video-leccion ]">
-		<ul class="slides">
-			<li>
-				
-				<div class="[ main-banner ][ height---100 ]" style="background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1463214551910-9d4d4e4ee844.jpg)">
-					<div class="[ gradient-linear-opacity ][ height---100 ][ padding-bottom ]">
-						<div class="[ container ]">
-							<div class="[ row ]">
-								<div class="[ col s12 ][ white-text text-center ]">
-									<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
-									<!-- Modal Trigger -->
-									<a class="[ block ][ width--75 ][ margin-auto ] waves-effect waves-light modal-trigger" href="#modal1"><img src="<?php echo THEMEPATH; ?>icons/play-button.png" alt="play button"></a>
+					<div class="[ main-banner ][ height---100 ]" style="background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1463214551910-9d4d4e4ee844.jpg)">
+						<div class="[ gradient-linear-opacity ][ height---100 ][ padding-bottom ]">
+							<div class="[ container ]">
+								<div class="[ row ]">
+									<div class="[ col s12 ][ white-text text-center ]">
+										<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
+										<!-- Modal Trigger -->
+										<a class="[ block ][ width--75 ][ margin-auto ] waves-effect waves-light modal-trigger" href="#modal1"><img src="<?php echo THEMEPATH; ?>icons/play-button.png" alt="play button"></a>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</li>
-			<li>
-				<div class="[ main-banner ][ height---100 ]" style="background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg)">
-					<div class="[ gradient-linear-opacity ][ height---100 ][ padding-bottom ]">
-						<div class="[ container ]">
-							<div class="[ row ]">
-								<div class="[ col s12 ][ white-text text-center ]">
-									<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
-									<!-- Modal Trigger -->
-									<a class="[ block ][ width--75 ][ margin-auto ] waves-effect waves-light modal-trigger" href="#modal2"><img src="<?php echo THEMEPATH; ?>icons/play-button.png" alt="play button"></a>
+				</li>
+				<li>
+					<div class="[ main-banner ][ height---100 ]" style="background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg)">
+						<div class="[ gradient-linear-opacity ][ height---100 ][ padding-bottom ]">
+							<div class="[ container ]">
+								<div class="[ row ]">
+									<div class="[ col s12 ][ white-text text-center ]">
+										<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
+										<!-- Modal Trigger -->
+										<a class="[ block ][ width--75 ][ margin-auto ] waves-effect waves-light modal-trigger" href="#modal2"><img src="<?php echo THEMEPATH; ?>icons/play-button.png" alt="play button"></a>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</li>
-			<li>
-				<div class="[ main-banner ][ height---100 ]" style="background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1462657634725-85ccd06716a6.jpg)">
-					<div class="[ gradient-linear-opacity ][ height---100 ][ padding-bottom ]">
-						<div class="[ container ]">
-							<div class="[ row ]">
-								<div class="[ col s12 ][ white-text text-center ]">
-									<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
-									<!-- Modal Trigger -->
-									<a class="[ block ][ width--75 ][ margin-auto ] waves-effect waves-light modal-trigger" href="#modal1"><img src="<?php echo THEMEPATH; ?>icons/play-button.png" alt="play button"></a>
+				</li>
+				<li>
+					<div class="[ main-banner ][ height---100 ]" style="background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1462657634725-85ccd06716a6.jpg)">
+						<div class="[ gradient-linear-opacity ][ height---100 ][ padding-bottom ]">
+							<div class="[ container ]">
+								<div class="[ row ]">
+									<div class="[ col s12 ][ white-text text-center ]">
+										<h1 class="[ padding-sides ]">Yoga cloud cursos</h1>
+										<!-- Modal Trigger -->
+										<a class="[ block ][ width--75 ][ margin-auto ] waves-effect waves-light modal-trigger" href="#modal1"><img src="<?php echo THEMEPATH; ?>icons/play-button.png" alt="play button"></a>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</li>
-		</ul>
-	</div>
-
-	<!-- Modal Structure 1-->
-	<div id="modal1" class="modal">
-		<div class="modal-header">
-			<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat"><img src="<?php echo THEMEPATH; ?>icons/Close.png" alt="menu"></a>
+				</li>
+			</ul>
 		</div>
-		<div class="modal-content [ video-leccion ]">
-			<div class="video-container [ width---100 ]">
-				<iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+
+		<!-- Modal Structure 1-->
+		<div id="modal1" class="modal">
+			<div class="modal-header">
+				<a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat"><img src="<?php echo THEMEPATH; ?>icons/Close.png" alt="menu"></a>
+			</div>
+			<div class="modal-content [ video-leccion ]">
+				<div class="video-container [ width---100 ]">
+					<iframe width="853" height="480" src="//www.youtube.com/embed/Q8TXgCzxEnw?rel=0" frameborder="0" allowfullscreen></iframe>
+				</div>
 			</div>
 		</div>
-	</div>
 
-	<div class="[ row ][ text-center ]">
-		<div class="[ col s6 ][ bg-primary ][ border-right--light ][ waves-effect waves-light ]">
-			<a href="" id="prev" class="[ padding-vertical--small ][ white-text ][ block ]">
-				<img class="[ margin-right ][ inline-block middle ]" src="<?php echo THEMEPATH; ?>icons/left-arrow.png" alt="">
-				<span class="[ inline-block middle ]">anterior</span>
-			</a>
+		<div class="[ row ][ text-center ]">
+			<div class="[ col s6 ][ bg-primary ][ border-right--light ][ waves-effect waves-light ]">
+				<a href="" id="prev" class="[ padding-vertical--small ][ white-text ][ block ]">
+					<img class="[ margin-right ][ inline-block middle ]" src="<?php echo THEMEPATH; ?>icons/left-arrow.png" alt="">
+					<span class="[ inline-block middle ]">anterior</span>
+				</a>
+			</div>
+			<div class="[ col s6 ][ bg-primary ][ waves-effect waves-light ]">
+				<a href="" id="next" class="[ padding-vertical--small ][ white-text ][ block ]">
+					<span class="[ inline-block middle ]">siguiente</span>
+					<img class="[ margin-left ][ inline-block middle ]" src="<?php echo THEMEPATH; ?>icons/right-arrow.png" alt="">
+				</a>
+			</div>
 		</div>
-		<div class="[ col s6 ][ bg-primary ][ waves-effect waves-light ]">
-			<a href="" id="next" class="[ padding-vertical--small ][ white-text ][ block ]">
-				<span class="[ inline-block middle ]">siguiente</span>
-				<img class="[ margin-left ][ inline-block middle ]" src="<?php echo THEMEPATH; ?>icons/right-arrow.png" alt="">
-			</a>
-		</div>
-	</div>
+	</section>
 
 	<section class="[ container ]">
 		<article class="[ content-user ]">
@@ -84,8 +85,8 @@
 			<p>Verum hoc loco sumo verbis his eandem certe vim voluptatis Epicurum nosse quam ceteros. Si enim ad populum me vocas, eum. Sed residamus, inquit, si placet. Cave putes quicquam esse verius. Duo Reges: constructio interrete.</p>
 			<img src="<?php echo THEMEPATH; ?>images/sky.png" alt="sky image">
 			<p>Portenta haec esse dicit, neque ea ratione ullo modo posse vivi; Quid, si non sensus modo ei sit datus, verum etiam animus hominis? Quamvis enim depravatae non sint, pravae tamen esse possunt. Egone quaeris, inquit, quid sentiam? [redacted]tilio Rufo.</p>
-			<iframe src="https://player.vimeo.com/video/97989105?autoplay=1&color=00ddb3&title=0&byline=0&portrait=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>	
-			<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/256340512&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
+			<iframe src="https://player.vimeo.com/video/97989105?autoplay=1&color=00ddb3&title=0&byline=0&portrait=0" width="100%" height="100%" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+			<iframe width="100%" height="150" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/256340512&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;visual=false"></iframe>
 		</article>
 		<div class="[ text-center ][ margin-bottom--large ]">
 			<a class="[ btn btn-rounded ][ waves-effect waves-light ]">
