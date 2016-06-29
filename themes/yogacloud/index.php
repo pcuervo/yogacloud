87b4dac7b8a63517f58ee3fdb785819446a517e2
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-29 13:04:29 -0500

    cuadros de curso en index con nuevas medidas y clases

--- themes/yogacloud/index.php
+++ themes/yogacloud/index.php
@@ -24,88 +24,68 @@
 	<section class="[ container ]">
 		<div class="[ row ]">
 			<article class="[ col s12 m6 ]">
-				<div class="[ card ][ row ]">
-					<div class="[ card-image ][ col s12 l6 ][ no-padding-sides ]">
-						<div style="height: 200px; width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg)">
-							<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
-								<span class="[ card-title ]">Título del curso</span>
+				<div class="[ card ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div style="height: 200px; width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
+									<span class="[ card-title ]">Título del curso</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="[ col l6 ]">
-						<div class="[ card-content ][ text-ellipsis ]" id="cursos">
-							<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-						</div>
-						<div class="[ relative ][ top--15 ][ text-center ]">
-							<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis ]" id="cursos">
+								<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+							</div>
+							<div class="[ relative ][ top--15 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</article>
 			<article class="[ col s12 m6 ]">
-				<div class="card">
-					<div class="[ card-image ][ col l6 ]">
-						<div style="height: 200px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1463214551910-9d4d4e4ee844.jpg)">
-							<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
-								<span class="[ card-title ]">Título del curso</span>
+				<div class="[ card ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div style="height: 200px; width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
+									<span class="[ card-title ]">Título del curso</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="[ col l6 ]">
-						<div class="[ bg-secondary ][ text-center ][ white-text ]">
-							<p class="[ no-margin padding--xsmall ]">NUEVO</p>
-						</div>
-						<div class="[ card-content ]">
-						<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing</p>
-						</div>
-						<div class="[ relative ][ top--15 ][ text-center ]">
-							<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis ]" id="cursos">
+								<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+							</div>
+							<div class="[ relative ][ top--15 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</article>
 			<article class="[ col s12 m6 offset-m3 ]">
-				<div class="card">
-					<div class="[ card-image ][ col l6 ]">
-						<div style="height: 200px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1463214551910-9d4d4e4ee844.jpg)">
-							<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
-								<span class="[ card-title ]">Título del curso</span>
+				<div class="[ card ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div style="height: 200px; width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1445384763658-0400939829cd.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
+									<span class="[ card-title ]">Título del curso</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div class="[ col l6 ]">
-						<div class="[ bg-secondary-darken-10 ][ text-center ][ white-text ][ text-uppercase ]">
-							<p class="[ no-margin padding--xsmall ]">Destacado</p>
-						</div>
-						<div class="[ card-content ]">
-							<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing</p>
-						</div>
-						<div class="[ relative ][ top--15 ][ text-center ]">
-							<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis ]" id="cursos">
+								<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+							</div>
+							<div class="[ relative ][ top--15 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
+							</div>
 						</div>
 					</div>
 				</div>
 			</article>
-			<!-- <article class="[ col s12 ]">
-				<div class="card">
-					<div class="[ card-image ]">
-						<div style="height: 200px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1463214551910-9d4d4e4ee844.jpg)">
-							<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ]">
-								<span class="[ card-title ]">Título del curso</span>
-							</div>
-						</div>
-					</div>
-					<div class="[ bg-secondary-darken-20 ][ text-center ][ white-text ][ text-uppercase ]">
-						<p class="[ no-margin padding--xsmall ]">Proximamente</p>
-					</div>
-					<div class="[ card-content ]">
-					<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing</p>
-					</div>
-					<div class="[ relative ][ top--15 ][ text-center ]">
-						<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">más info</a>
-					</div>
-				</div>
-			</article> -->
 		</div>
 	</section>
 
