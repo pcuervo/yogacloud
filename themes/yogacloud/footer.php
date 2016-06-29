b6f6f1ffea029adfc7c5d9b6c8e72a75b0445692
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-29 10:25:03 -0500

    columnas en footer para desktop

--- themes/yogacloud/footer.php
+++ themes/yogacloud/footer.php
@@ -2,12 +2,12 @@
 		<footer class="page-footer">
 			<div class="container">
 				<div class="row [ border-bottom--light ][ padding-bottom ]">
-					<div class="col l4 s12 [ margin-bottom ]">
+					<div class="col s12 offset-m1 m5 offset-l2 l4 [ margin-bottom ]">
 						<h5 class="white-text [ no-margin-top ]">¿Necesitas ayuda?</h5>
 						<a href="tel:+525552555555" class="[ white-text ]"><i class="[ icon-phone-circle-1 icon-large padding-sides--xsmall ]"></i></a>
 						<a href="mailto:contacto@yogacloud.com" class="[ white-text ]"><i class="[ icon-email-circle-1 icon-large padding-sides--xsmall ]"></i></a>
 					</div>
-					<div class="col l4 s12">
+					<div class="col s12 m5 l4">
 						<h5 class="white-text [ no-margin-top ]">Seámos amigos</h5>
 						<a href="" class="[ white-text ]"><i class="[ icon-twitter icon-medium padding-sides--xsmall ]"></i></a>
 						<a href="" class="[ white-text ]"><i class="[ icon-facebook icon-iconed padding-sides--xsmall ]"></i></a>
@@ -15,20 +15,23 @@
 						<img src="" alt="">
 					</div>
 				</div>
-				<div class="col l4 s12">
-					<h5><a href="<?php echo site_url('/'); ?>"><img class="logo center-block" src="<?php echo THEMEPATH; ?>images/logo-horizontal-light.png" alt="Logo yogacloud"></a></h5>
-					<div class="[ row ]">
-						<div class="col s4">
-							<a class="grey-text text-lighten-3" href="<?php echo site_url('/aviso-legal/'); ?>">Aviso Legal</a>
-						</div>
-						<div class="col s4">
-							<a class="grey-text text-lighten-3" href="<?php echo site_url('/politica-de-cookies/'); ?>">Política de Cookies</a>
-						</div>
-						<div class="col s4">
-							<a class="grey-text text-lighten-3" href="<?php echo site_url('/politica-de-privacidad/'); ?>">Política de Privacidad</a>
+				<div class="[ row ]">
+					<div class="col s12 offset-l2 l8">
+						<h5><a href="<?php echo site_url('/'); ?>"><img class="logo center-block" src="<?php echo THEMEPATH; ?>images/logo-horizontal-light.png" alt="Logo yogacloud"></a></h5>
+						<div class="[ row ]">
+							<div class="col s4">
+								<a class="grey-text text-lighten-3" href="<?php echo site_url('/aviso-legal/'); ?>">Aviso Legal</a>
+							</div>
+							<div class="col s4">
+								<a class="grey-text text-lighten-3" href="<?php echo site_url('/politica-de-cookies/'); ?>">Política de Cookies</a>
+							</div>
+							<div class="col s4">
+								<a class="grey-text text-lighten-3" href="<?php echo site_url('/politica-de-privacidad/'); ?>">Política de Privacidad</a>
+							</div>
 						</div>
 					</div>
 				</div>
+
 				<div class="[ white-text ]">
 					© Copyright 2016
 				</div>
