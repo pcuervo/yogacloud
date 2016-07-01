499582c372c5183ed822617e18381454f2299a52
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-06-30 19:22:47 -0500

    columnas para tienda en desktop

--- themes/yogacloud/page-tienda.php
+++ themes/yogacloud/page-tienda.php
@@ -17,56 +17,59 @@
 	</section>
 	<section class="[ container ]">
 		<h5 class="[ text-center ][ margin-bottom ]">Lo más vendido</h5>
-		<article class="[ box-btn--middle ]">
-			<div class="[ card-image ][ relative ]">
-				<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda1.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
-						<span class="[ title-image ]">Título del producto</span>
+		<div class="[ row ]">
+			<article class="[ col s12 m6 ][ box-btn--middle ]">
+				<div class="[ card-image ][ relative ]">
+					<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda1.png)">
+						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
+							<span class="[ title-image ]">Título del producto</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="[ relative ][ bottom--22 ][ text-center ]">
-				<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
-			</div>
-		</article>
-		<article class="[ box-btn--middle ]">
-			<div class="[ card-image ][ relative ]">
-				<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda2.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
-						<span class="[ title-image ]">Título del producto</span>
+				<div class="[ relative ][ bottom--22 ][ text-center ]">
+					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
+				</div>
+			</article>
+			<article class="[ col s12 m6 ][ box-btn--middle ]">
+				<div class="[ card-image ][ relative ]">
+					<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda2.png)">
+						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
+							<span class="[ title-image ]">Título del producto</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="[ relative ][ bottom--22 ][ text-center ]">
-				<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
-			</div>
-		</article>
-		<article class="[ box-btn--middle ]">
-			<div class="[ card-image ][ relative ]">
-				<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda3.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
-						<span class="[ title-image ]">Título del producto</span>
+				<div class="[ relative ][ bottom--22 ][ text-center ]">
+					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
+				</div>
+			</article>
+			<article class="[ col s12 m6 ][ box-btn--middle ]">
+				<div class="[ card-image ][ relative ]">
+					<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda3.png)">
+						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
+							<span class="[ title-image ]">Título del producto</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="[ relative ][ bottom--22 ][ text-center ]">
-				<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
-			</div>
-		</article>
-		<article class="[ box-btn--middle ]">
-			<div class="[ card-image ][ relative ]">
-				<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
-						<span class="[ title-image ]">Título del producto</span>
+				<div class="[ relative ][ bottom--22 ][ text-center ]">
+					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
+				</div>
+			</article>
+			<article class="[ col s12 m6 ][ box-btn--middle ]">
+				<div class="[ card-image ][ relative ]">
+					<div style="height: 322px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
+						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
+							<span class="[ title-image ]">Título del producto</span>
+						</div>
 					</div>
 				</div>
-			</div>
-			<div class="[ relative ][ bottom--22 ][ text-center ]">
-				<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
-			</div>
-		</article>
+				<div class="[ relative ][ bottom--22 ][ text-center ]">
+					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
+				</div>
+			</article>
+		</div>
+
 	</section>
-	<section class="[ margin-bottom ]">
+	<section class="[ margin-bottom ][ container--on-med-and-up ]">
 		<article class="[ gradient-diagonal ][ padding ]">
 			<h5 class="[ text-center ][ white-text ][ margin-bottom ]">Las más mas exclusivas</h5>
 			<div class="[ row ][ margin-bottom ]">
@@ -118,42 +121,51 @@
 				</div>
 			</div>
 		</article>
-		<article>
-			<div class="[ card-image ][ relative ][ text-center ]">
-				<div style="height: 420px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ][ padding-sides padding-vertical--xlarge ][ valign-wrapper ]">
-						<div class="[ valign ][ width---100 ]">
-							<h4 class="[ white-text ][ no-margin-top ]">Descuentos en calzado</h4>
-							<a class="[ btn btn-rounded btn-light ][ margin-auto ][ width--170 ][ valign ]">ver productos</a>
+		<div class="[ row ]">
+			<article class="[ col s12 l6 ]">
+				<div class="[ card-image ][ relative ][ text-center ]">
+					<div style="height: 440px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
+						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ][ padding-sides padding-vertical--xlarge ][ valign-wrapper ]">
+							<div class="[ valign ][ width---100 ]">
+								<h4 class="[ white-text ][ no-margin-top ]">Descuentos en calzado</h4>
+								<a class="[ btn btn-rounded btn-light ][ margin-auto ][ width--170 ][ valign ]">ver productos</a>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</article>
-		<article>
-			<div class="[ card-image ][ relative ][ text-center ]">
-				<div style="height: 220px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ][ padding-sides padding-vertical ][ valign-wrapper ]">
-						<div class="[ valign ][ width---100 ]">
-							<h4 class="[ white-text ][ width---100 ][ no-margin-top ]">20% de descuento en mats</h4>
-							<a class="[ btn btn-rounded btn-light ]">ver productos</a>
+			</article>
+			<div class="[ row ]">
+				<div class="[ col s12 l6 ]">
+					<article>
+						<div class="[ card-image ][ relative ][ text-center ]">
+							<div style="height: 210px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
+								<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ][ padding-sides padding-vertical ][ valign-wrapper ]">
+									<div class="[ valign ][ width---100 ]">
+										<h4 class="[ white-text ][ width---100 ][ no-margin-top ]">20% de descuento en mats</h4>
+										<a class="[ btn btn-rounded btn-light ]">ver productos</a>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
-				</div>
-			</div>
-		</article>
-		<article>
-			<div class="[ card-image ][ relative ][ text-center ]">
-				<div style="height: 220px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
-					<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ][ padding-sides padding-vertical ][ valign-wrapper ]">
-						<div class="[ valign ][ width---100 ]">
-							<h4 class="[ white-text ][ width---100 ][ no-margin-top ]">10% en toda la tienda con el código</h4>
-							<a class="[ btn btn-rounded btn-light ]">yogclouder16</a>
+					</article>
+					<article>
+						<div class="[ card-image ][ relative ][ text-center ]">
+							<div style="height: 210px; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
+								<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ][ padding-sides padding-vertical ][ valign-wrapper ]">
+									<div class="[ valign ][ width---100 ]">
+										<h4 class="[ white-text ][ width---100 ][ no-margin-top ]">10% en toda la tienda con el código</h4>
+										<a class="[ btn btn-rounded btn-light ]">yogclouder16</a>
+									</div>
+								</div>
+							</div>
 						</div>
-					</div>
+					</article>
 				</div>
+
 			</div>
-		</article>
+
+		</div>
+
 	</section>
 
 <?php get_footer(); ?>
