60146444fe2b99e325ac53cd8a3fdf091fc7833c
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-07-04 11:40:10 -0500

    height-testimonial solo en un slider

--- themes/yogacloud/js/functions.js
+++ themes/yogacloud/js/functions.js
@@ -186,9 +186,8 @@ function boxCard(){
 }
 
 function sliderHeight(){
-    //image size
-    var caption_height = $('.testimonials .caption').height();
+    var caption_height = $('.testimonials li.active .caption').height();
     var testimonial_height = (caption_height + 20);
     console.log('Alto slider:', testimonial_height + 'px');
-    $('.height-testimonial').css('height', "testimonial_height" );
+    $('.height-testimonials').css('height', testimonial_height );
 }
