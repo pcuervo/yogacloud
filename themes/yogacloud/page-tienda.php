4e4337626d602d20dd733b5b0120c8936d119d33
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-07-04 13:07:58 -0500

    correccion de sintaxis en columnas de tienda

--- themes/yogacloud/page-tienda.php
+++ themes/yogacloud/page-tienda.php
@@ -18,7 +18,7 @@
 	<section class="[ container ]">
 		<h5 class="[ text-center ][ margin-bottom ]">Lo más vendido</h5>
 		<div class="[ row ]">
-			<article id="box-card" class="[ col s12 m6pproductos ][ box-btn--middle ]">
+			<article id="box-card" class="[ col s12 m6 productos ][ box-btn--middle ]">
 				<div class="[ card-image ][ relative ]">
 					<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda1.png)">
 						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
@@ -30,7 +30,7 @@
 					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
 				</div>
 			</article>
-			<article id="box-card" class="[ col s12 m6pproductos ][ box-btn--middle ]">
+			<article id="box-card" class="[ col s12 m6 productos ][ box-btn--middle ]">
 				<div class="[ card-image ][ relative ]">
 					<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda2.png)">
 						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
@@ -42,7 +42,7 @@
 					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
 				</div>
 			</article>
-			<article id="box-card" class="[ col s12 m6pproductos ][ box-btn--middle ]">
+			<article id="box-card" class="[ col s12 m6 productos ][ box-btn--middle ]">
 				<div class="[ card-image ][ relative ]">
 					<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda3.png)">
 						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
@@ -54,7 +54,7 @@
 					<a class="[ btn btn-rounded ][ waves-effect waves-light ]">comprar - $900</a>
 				</div>
 			</article>
-			<article id="box-card" class="[ col s12 m6pproductos ][ box-btn--middle ]">
+			<article id="box-card" class="[ col s12 m6 productos ][ box-btn--middle ]">
 				<div class="[ card-image ][ relative ]">
 					<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/tienda4.png)">
 						<div class="[ gradient-linear-opacity--light-2 ][ width---100 height---100 ]">
