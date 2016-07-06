b65cbb7ecd074473c0db0e69c119b4b8ab659e3e
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-07-06 14:04:11 -0500

    corrección de varios ids cursos en index

--- themes/yogacloud/index.php
+++ themes/yogacloud/index.php
@@ -36,7 +36,7 @@
 							</div>
 						</div>
 						<div class="[ col l6 ]">
-							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]" id="cursos">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
 								<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 							</div>
 							<div class="[ relative ][ top--22 ][ text-center ]">
@@ -59,7 +59,7 @@
 							</div>
 						</div>
 						<div class="[ col l6 ]">
-							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]" id="cursos">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
 								<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 							</div>
 							<div class="[ relative ][ top--22 ][ text-center ]">
@@ -81,7 +81,7 @@
 							</div>
 						</div>
 						<div class="[ col l6 ]">
-							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]" id="cursos">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
 								<p>Diodorus eius auditor adiungit ad honestatem vacuitatem doloris quod non faceret si in voluptate summum bonum poneret non igitur bene verba tu fingas et ea dicas quae non sentias nam. specializing. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 							</div>
 							<div class="[ relative ][ top--22 ][ text-center ]">
