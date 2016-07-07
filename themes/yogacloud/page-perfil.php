1d52cabd2b9d972a004a142d88a983b0181b3328
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-07-07 10:21:32 -0500

    perfil usuaerio con cursos

--- themes/yogacloud/page-perfil.php
+++ themes/yogacloud/page-perfil.php
@@ -1,15 +1,15 @@
 <?php get_header(); the_post();  ?>
-	<section class="">
+	<section style="background-color: #f8f8f8;">
 		<div class="[ container ][ padding-vertical ]">
-			<div class="[ row ]">
+			<div class="[ row ][ no-margin ]">
 				<div class="[ col s6 ][ text-center ][ border-right--primary ]">
 					<img src="<?php echo THEMEPATH; ?>images/testimonial.png" alt="image user">
 					<h5>Raúl de Zamacona</h5>
 					<p><strong class="[ color-primary ]">Email: </strong>raul@email.com</p>
 					<p><strong class="[ color-primary ]">Contraseña: </strong>*********</p>
-					<ul class="collapsible" data-collapsible="accordion">
+					<ul class="collapsible [ no-margin ]" data-collapsible="accordion">
 						<li>
-							<div class="collapsible-header">
+							<div class="collapsible-header" style="background-color: #f8f8f8;">
 								<a class="[ btn btn-rounded btn-primary-hollow waves-effect waves-light ][ btn-small ]">editar</a>
 							</div>
 							<div class="collapsible-body">
@@ -43,15 +43,145 @@
 					</ul>
 				</div>
 				<div class="[ col s6 ]">
-					<h5 class="[ text-center ][ margin-bottom ]">Badges</h5>
-					<p>Curso básico de Yoga</p>
+					<h5 class="[ text-center ][ margin-bottom ]">Cursos completados</h5>
+					<p><i class="[ icon icon-badge-star-1 icon-small ][ color-primary ]"></i> Curso básico de Yoga</p>
+					<p><i class="[ icon icon-badge-star-1 icon-small ][ color-primary ]"></i> Curso intermedio de Yoga</p>
 				</div>
 			</div>
 		</div>
 	</section>
 	<section class="[ container ]">
-		<h5 class="[ text-center ]">Mis cursos</h5>
-
+		<h5 class="[ text-center ][ margin-bottom ]">Mis cursos</h5>
+		<div class="[ row ]">
+			<article class="[ col s12 m6 ][ margin-bottom ]">
+				<div class="[ row ][ margin-bottom--small ]">
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Módulo: <span class="[ font-size--20 ]">3 de 5</span></h6>
+					</div>
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Lección: <span class="[ font-size--20 ]">3 de 4</span></h6>
+					</div>
+				</div>
+				<div class="progress">
+					<div class="determinate" style="width: 90%"></div>
+				</div>
+				<div id="box-card" class="[ card ][ margin-bottom--large ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ][ relative ]">
+									<span class="[ card-title ]">Curso dolor sit</span>
+								</div>
+							</div>
+						</div>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae debitis nostrum, illum nulla tenetur, officiis illo in, fuga consequatur officia porro qui. Ipsam excepturi nam ratione amet, cum quas maxime! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							</div>
+							<div class="[ relative ][ top--22 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">ver curso</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+			<article class="[ col s12 m6 ][ margin-bottom ]">
+				<div class="[ row ][ margin-bottom--small ]">
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Módulo: <span class="[ font-size--20 ]">3 de 5</span></h6>
+					</div>
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Lección: <span class="[ font-size--20 ]">3 de 4</span></h6>
+					</div>
+				</div>
+				<div class="progress">
+					<div class="determinate" style="width: 70%"></div>
+				</div>
+				<div id="box-card" class="[ card ][ margin-bottom--large ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ][ relative ]">
+									<span class="[ card-title ]">Curso dolor sit</span>
+								</div>
+							</div>
+						</div>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae debitis nostrum, illum nulla tenetur, officiis illo in, fuga consequatur officia porro qui. Ipsam excepturi nam ratione amet, cum quas maxime! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							</div>
+							<div class="[ relative ][ top--22 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">ver curso</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+			<article class="[ col s12 m6 ][ margin-bottom ]">
+				<div class="[ row ][ margin-bottom--small ]">
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Módulo: <span class="[ font-size--20 ]">3 de 5</span></h6>
+					</div>
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Lección: <span class="[ font-size--20 ]">3 de 4</span></h6>
+					</div>
+				</div>
+				<div class="progress">
+					<div class="determinate" style="width: 50%"></div>
+				</div>
+				<div id="box-card" class="[ card ][ margin-bottom--large ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ][ relative ]">
+									<span class="[ card-title ]">Curso dolor sit</span>
+								</div>
+							</div>
+						</div>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae debitis nostrum, illum nulla tenetur, officiis illo in, fuga consequatur officia porro qui. Ipsam excepturi nam ratione amet, cum quas maxime! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							</div>
+							<div class="[ relative ][ top--22 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">ver curso</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+			<article class="[ col s12 m6 ][ margin-bottom ]">
+				<div class="[ row ][ margin-bottom--small ]">
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Módulo: <span class="[ font-size--20 ]">3 de 5</span></h6>
+					</div>
+					<div class="[ col s12 l6 ]">
+						<h6 class="[ margin-bottom--xsmall ]">Lección: <span class="[ font-size--20 ]">3 de 4</span></h6>
+					</div>
+				</div>
+				<div class="progress">
+					<div class="determinate" style="width: 20%"></div>
+				</div>
+				<div id="box-card" class="[ card ][ margin-bottom--large ]">
+					<div class="[ row ]">
+						<div class="[ card-image ][ col s12 l6 ]">
+							<div class="[ bg-image--rectangle ]" style="width: 100%; background-size: cover; background-position: center bottom; background-image: url(<?php echo THEMEPATH; ?>images/photo-1464507768659-af94c4614d1a.jpg)">
+								<div class="[ gradient-linear-opacity--light ][ width---100 height---100 ][ relative ]">
+									<span class="[ card-title ]">Curso dolor sit</span>
+								</div>
+							</div>
+						</div>
+						<div class="[ col l6 ]">
+							<div class="[ card-content ][ text-ellipsis height-box-ellipsis ]">
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae debitis nostrum, illum nulla tenetur, officiis illo in, fuga consequatur officia porro qui. Ipsam excepturi nam ratione amet, cum quas maxime! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+							</div>
+							<div class="[ relative ][ top--22 ][ text-center ]">
+								<a href="<?php echo site_url('/curso/'); ?>" class="[ btn btn-rounded waves-effect waves-light ]">ver curso</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</article>
+		</div>
 	</section>
 
 <?php get_footer(); ?>
