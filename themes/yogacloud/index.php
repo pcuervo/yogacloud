9faa4e5b12a337944333dd2315469d74cfd5cd45
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-07-12 13:56:17 -0500

    tamaño ellipsis

--- themes/yogacloud/index.php
+++ themes/yogacloud/index.php
@@ -60,7 +60,7 @@
 									</div>
 								</div>
 								<div class="[ col s12 xl6 ]">
-									<div class="[ card-content ][ ellipsis-height ][ overflow-hidden ]">
+									<div class="[ card-content ][  ][ overflow-hidden ]">
 										<?php the_excerpt(); ?>
 										<?php ?>
 									</div>
