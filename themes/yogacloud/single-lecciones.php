2cd151e89a6f8d2e6d40791e87ffd002791888bc
Raúl De Zamacona <raulppcuervo.com>
2016-07-15 13:56:03 -0500

    curso competado

--- themes/yogacloud/single-lecciones.php
+++ themes/yogacloud/single-lecciones.php
@@ -37,9 +37,13 @@
 		</article>
 	<?php endif; ?>
 
+	<div class="[ bg-primary ][ padding-vertical ][ course-completed ]">
+		<h6 class="[ white-text ][ no-margin ]"><strong>¡Haz completado el curso!</strong><i class="[ icon icon-badge-star-1 icon-medium ][ color-light ]"></i></h6>
+	</div>
+
 	<?php if ( $leccion->has_been_watched_by_user( get_current_user_id() ) ) : ?>
 		<div class="[ bg-secondary ][ padding-vertical--xsmall ][ lesson-completed ]">
-			<h6 class="[ white-text ][ no-margin ]"><small>Lección completada</small><i class="[ icon icon-badge-star-1 icon--small ][ color-light ]"></i></h6>
+			<h6 class="[ white-text ][ no-margin ]"><small>Lección completada</small><i class="[ icon icon-badge-star-1 icon-xsmall ][ color-light ]"></i></h6>
 		</div>
 	<?php else : ?>
 		<div class="[ bg-secondary ][ padding-vertical--xsmall ][ transition not-visible ][ lesson-completed js-lesson-completed ]">
