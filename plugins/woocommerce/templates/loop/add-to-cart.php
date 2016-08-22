4da091d820b414f99b5019683295f0f03b3cfc87
Nayeli-Jordan <nayeli.jordan16@gmail.com>
2016-08-22 09:41:18 -0500

    estilo select variaciones

--- plugins/woocommerce/templates/loop/add-to-cart.php
+++ plugins/woocommerce/templates/loop/add-to-cart.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-	sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">COMPRAR - </a>',
+	sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">COMPRAR -</a>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( isset( $quantity ) ? $quantity : 1 ),
 		esc_attr( $product->id ),
@@ -32,3 +32,5 @@ echo apply_filters( 'woocommerce_loop_add_to_cart_link',
 		esc_html( $product->add_to_cart_text() )
 	),
 $product );
+
+
